1ec3c18306ae43e34d8bb99fda6666c7829cd6b8
Sumant Bhaskaruni <bsumantb@gmail.com>
2017-04-15 19:00:46 -0400

    Find all functions and store their properties

--- doc-writer.py
+++ doc-writer.py
@@ -2,6 +2,7 @@
 # -*- coding: utf -8 -*-
 
 from argparse import *
+import ast
 
 parser = ArgumentParser(description = 'Generate docstrings for a Python '
                         'script.')
@@ -9,3 +10,14 @@ parser.add_argument('filename', help = 'The name of the script to generate '
                     'docstrings for.')
 args = parser.parse_args()
 script = args.filename
+
+functions = []
+
+with open(script) as f:
+    node = ast.parse(f.read())
+    for i in node.body:
+        if isinstance(i, ast.FunctionDef):
+            function_name = i.name
+            function_args = [arg.arg for arg in i.args.args]
+
+            functions.append({'name': function_name, 'args': function_args})
